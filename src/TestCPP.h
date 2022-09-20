48ec340ff9aa113348c5eb529cda9ec09c9a1f9d
David Elvig <David.Elvig@gmail.com>
2022-09-20 10:07:53 -0500

    Added, and commented out, C++ class

---
+++ src/TestCPP.h
@@ -0,0 +1,12 @@
+
+// class foo {
+
+// public:
+//     foo(int bar)   {
+//         _bar = bar;
+//     };
+//     int getSquareTimesBar(int i) { return i * i * _bar;    }
+
+// private:
+//     int _bar;
+// }
